3561d6992c156c675b1e26528fdba959859cd23e
k_ssen <20183019@kookmin.ac.kr>
2020-06-03 09:03:07 +0900

    kse페이지

--- Project_KAJ/Project_KAJ/urls.py
+++ Project_KAJ/Project_KAJ/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 import main.views
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('', main.views.home, name ='home'),
     path('accounts/',include('accounts.urls')),
     path('main/',include('main.urls')),
+    path('seeun/',main.views.seeun, name ='seeun'),
 ]
